2c3ca5d1f602074383d7fc6b03c5139bd5e13e16
liwei766 <uccblw@gmail.com>
2021-02-15 17:48:41 +0900

    init

--- youlai-auth/src/main/java/com/youlai/auth/controller/AuthController.java
+++ youlai-auth/src/main/java/com/youlai/auth/controller/AuthController.java
@@ -10,9 +10,6 @@ import com.youlai.common.constant.GlobalConstants;
 import com.youlai.common.result.Result;
 import com.youlai.common.result.ResultCode;
 import com.youlai.common.web.exception.BizException;
-import com.youlai.mall.ums.api.MemberFeignService;
-import com.youlai.mall.ums.pojo.UmsUser;
-import com.youlai.mall.ums.pojo.dto.AuthMemberDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -52,10 +49,6 @@ public class AuthController {
             @ApiImplicitParam(name = "username", defaultValue = "admin", value = "登录用户名"),
             @ApiImplicitParam(name = "password", defaultValue = "123456", value = "登录密码"),
 
-            // 微信小程序认证参数（无小程序可忽略）
-            @ApiImplicitParam(name = "code", value = "小程序code"),
-            @ApiImplicitParam(name = "encryptedData", value = "包括敏感数据在内的完整用户信息的加密数据"),
-            @ApiImplicitParam(name = "iv", value = "加密算法的初始向量"),
     })
     @PostMapping("/token")
     public Result postAccessToken(
@@ -63,71 +56,6 @@ public class AuthController {
             @ApiIgnore @RequestParam Map<String, String> parameters
     ) throws HttpRequestMethodNotSupportedException {
         String clientId = parameters.get(AuthConstants.JWT_CLIENT_ID_KEY);
-        switch (clientId) {
-            case AuthConstants.WEAPP_CLIENT_ID:  // 微信认证
-                return this.handleForWxAuth(principal, parameters);
-            default:
-                OAuth2AccessToken oAuth2AccessToken = tokenEndpoint.postAccessToken(principal, parameters).getBody();
-                Oauth2Token oauth2Token = Oauth2Token.builder()
-                        .token(oAuth2AccessToken.getValue())
-                        .refreshToken(oAuth2AccessToken.getRefreshToken().getValue())
-                        .expiresIn(oAuth2AccessToken.getExpiresIn())
-                        .build();
-                return Result.success(oauth2Token);
-        }
-    }
-
-
-    private WxMaService wxService;
-    private MemberFeignService memberFeignService;
-    private PasswordEncoder passwordEncoder;
-
-    public Result handleForWxAuth(Principal principal, Map<String, String> parameters) throws HttpRequestMethodNotSupportedException {
-
-        String code = parameters.get("code");
-
-        if (StrUtil.isBlank(code)) {
-            throw new BizException("code不能为空");
-        }
-
-        WxMaJscode2SessionResult session = null;
-        try {
-            session = wxService.getUserService().getSessionInfo(code);
-        } catch (WxErrorException e) {
-            e.printStackTrace();
-        }
-        String openid = session.getOpenid();
-        String sessionKey = session.getSessionKey();
-
-        Result<AuthMemberDTO> result = memberFeignService.getUserByOpenid(openid);
-
-        if (ResultCode.USER_NOT_EXIST.getCode().equals(result.getCode())) { // 微信授权登录 会员信息不存在时 注册会员
-            String encryptedData = parameters.get("encryptedData");
-            String iv = parameters.get("iv");
-
-            WxMaUserInfo userInfo = wxService.getUserService().getUserInfo(sessionKey, encryptedData, iv);
-            if (userInfo == null) {
-                throw new BizException("获取用户信息失败");
-            }
-            UmsUser user = new UmsUser()
-                    .setNickname(userInfo.getNickName())
-                    .setAvatar(userInfo.getAvatarUrl())
-                    .setGender(Integer.valueOf(userInfo.getGender()))
-                    .setOpenid(openid)
-                    .setUsername(openid)
-                    .setPassword(passwordEncoder.encode(openid).replace(AuthConstants.BCRYPT, Strings.EMPTY)) // 加密密码移除前缀加密方式 {bcrypt}
-                    .setStatus(GlobalConstants.STATUS_NORMAL_VALUE);
-
-            Result res = memberFeignService.add(user);
-            if (!ResultCode.SUCCESS.getCode().equals(res.getCode())) {
-                throw new BizException("注册会员失败");
-            }
-        }
-
-        // oauth2认证参数对应授权登录时注册会员的username、password信息，模拟通过oauth2的密码模式认证
-        parameters.put("username", openid);
-        parameters.put("password", openid);
-
         OAuth2AccessToken oAuth2AccessToken = tokenEndpoint.postAccessToken(principal, parameters).getBody();
         Oauth2Token oauth2Token = Oauth2Token.builder()
                 .token(oAuth2AccessToken.getValue())
@@ -136,6 +64,8 @@ public class AuthController {
                 .build();
         return Result.success(oauth2Token);
     }
+
+
 
 
 }
