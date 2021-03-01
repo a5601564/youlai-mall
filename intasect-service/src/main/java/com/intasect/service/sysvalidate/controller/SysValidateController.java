240233369da58069d5186713a5b320e02622c981
liwei766 <uccblw@gmail.com>
2021-03-01 09:59:12 +0900

    init done

--- intasect-service/src/main/java/com/intasect/service/sysvalidate/controller/SysValidateController.java
+++ intasect-service/src/main/java/com/intasect/service/sysvalidate/controller/SysValidateController.java
@@ -1,31 +1,20 @@
 package com.intasect.service.sysvalidate.controller;
 
-import cn.hutool.core.lang.UUID;
-import cn.hutool.json.JSONObject;
 import com.intasect.service.common.controller.CommonController;
-import com.intasect.service.common.entity.PageInfo;
 import com.intasect.service.sysvalidate.entity.SysValidate;
 import com.intasect.service.sysvalidate.entity.SysValidateVo;
 import com.intasect.service.sysvalidate.mapper.SysValidateMapper;
 import com.intasect.service.sysvalidate.service.SysValidateService;
-import com.intasect.service.util.CopyUtil;
-import com.youlai.admin.pojo.entity.SysUser;
-import com.youlai.admin.service.ISysUserService;
+import com.intasect.service.admin.pojo.entity.SysUser;
+import com.intasect.service.admin.service.ISysUserService;
 import com.youlai.common.result.Result;
 import com.youlai.common.result.ResultCode;
-import com.youlai.common.utils.UserUtil;
-import org.apache.commons.lang.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * <p>
